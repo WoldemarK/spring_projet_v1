0c65f2e5943cff7c1fe36ff298bf40966dae7529
WoldemarK <kovtynov.vladimir@gmail.com>
2023-07-31 17:17:48 +0300

    Превичная настройка проекта

---
+++ src/main/java/com/example/springprojetv1/SpringProjetV1Application.java
@@ -0,0 +1,13 @@
+package com.example.springprojetv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjetV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjetV1Application.class, args);
+    }
+
+}
